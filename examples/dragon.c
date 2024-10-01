5a123a85759603d3e3aa0132de8e254f87ddf71e
wk1093 <wyattk1093@gmail.com>
2024-10-01 10:20:27 -0600

    examples and stuff

--- examples/dragon.c
+++ examples/dragon.c
@@ -27,16 +27,90 @@ int main() {
     // time to load the model
     EogllBufferObject bufferObject = eogllLoadBufferObject("resources/models/dragon.obj", objMode, GL_STATIC_DRAW);
 
-    while (!eogllWindowShouldClose(window)) {
-        eogllRotateModel(&model, 50.0f, (vec3) {0.52f, 1.0f, 0.123f});
+    double lastTime = eogllGetTime();
 
+    double speed = 1.0;
+    double lastSpeed = 0.0;
+
+    double moveSpeed = 1.0;
+    double sensitivity = 0.1;
+
+    bool mouseDisabled = false;
+
+    while (!eogllWindowShouldClose(window)) {
+        double currentTime = eogllGetTime();
+        float deltaTime = (float)(currentTime - lastTime);
+        lastTime = currentTime;
+
+        eogllRotateModel(&model, 50.0f, (vec3) {5.2f*deltaTime*speed, 10.0f*deltaTime*speed, 1.23f*deltaTime*speed});
+
+        if (window->isDown[GLFW_KEY_LEFT]) {
+            speed -= deltaTime*5;
+        }
+        if (window->isDown[GLFW_KEY_RIGHT]) {
+            speed += deltaTime*5;
+        }
+        if (window->isDown[GLFW_KEY_UP]) {
+            moveSpeed += deltaTime*5;
+        }
+        if (window->isDown[GLFW_KEY_DOWN]) {
+            moveSpeed -= deltaTime*5;
+        }
+        if (window->press[GLFW_KEY_P]) {
+            if (lastSpeed == 0.0) {
+                lastSpeed = speed;
+                speed = 0.0;
+            } else {
+                speed = lastSpeed;
+                lastSpeed = 0.0;
+            }
+        }
+
+        if (window->isDown[GLFW_KEY_W]) {
+            eogllMoveCamera(&camera, EOGLL_FORWARD, deltaTime*moveSpeed);
+        }
+        if (window->isDown[GLFW_KEY_S]) {
+            eogllMoveCamera(&camera, EOGLL_BACKWARD, deltaTime*moveSpeed);
+        }
+        if (window->isDown[GLFW_KEY_A]) {
+            eogllMoveCamera(&camera, EOGLL_LEFT, deltaTime*moveSpeed);
+        }
+        if (window->isDown[GLFW_KEY_D]) {
+            eogllMoveCamera(&camera, EOGLL_RIGHT, deltaTime*moveSpeed);
+        }
+        if (window->isDown[GLFW_KEY_LEFT_SHIFT]) {
+            eogllMoveCamera(&camera, EOGLL_DOWN, deltaTime*moveSpeed);
+        }
+        if (window->isDown[GLFW_KEY_SPACE]) {
+            eogllMoveCamera(&camera, EOGLL_UP, deltaTime*moveSpeed);
+        }
+
+        if (window->mouseIsDown[GLFW_MOUSE_BUTTON_LEFT]) {
+            if (!mouseDisabled) {
+                mouseDisabled = true;
+                eogllSetCursorMode(window, EOGLL_CURSOR_DISABLED);
+            }
+        }
         if (window->isDown[GLFW_KEY_ESCAPE]) {
-            eogllSetWindowShouldClose(window, true);
+            if (mouseDisabled) {
+                mouseDisabled = false;
+                eogllSetCursorMode(window, EOGLL_CURSOR_NORMAL);
+            }
         }
-        if (window->press[GLFW_KEY_SPACE]) {
-            EOGLL_LOG_INFO(stdout, "Space pressed\n");
-            eogllSetWindowSize(window, 800, 600);
+
+        if (mouseDisabled) {
+            if (window->mousedx != 0) {
+                eogllYawCamera(&camera, window->mousedx * sensitivity);
+                window->mousedx = 0;
+            }
+            if (window->mousedy != 0) {
+                eogllPitchCamera(&camera, -window->mousedy * sensitivity);
+                window->mousedy = 0;
+            }
         }
+        
+
+
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // clear color buffer
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear depth buffer
 
